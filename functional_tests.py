d736b769f00cdef511f71d2597988c4819a65566
ROHIT RAWAT <rohit@spoton.com>
2019-09-01 09:42:43 -0500

    class based functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,21 @@
+import unittest
+
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
 
-assert "Django" in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.url = "http://localhost:8000"
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_enter_phone_number_and_get_result(self):
+        self.browser.get(self.url)
+        self.assertIn("Truecaller", self.browser.title)
+        self.fail("Test Finished")
+
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
